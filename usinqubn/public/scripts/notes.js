80f16d8103e2546c502edeac37ceaeec820fd6b0
Zulfadli77 <zulfadli7081@gmail.com>
2021-06-02 02:35:01 +0800

    add to do list instead of calendar ( maybe)

---
+++ usinqubn/public/scripts/notes.js
@@ -0,0 +1,63 @@
+var listItemID = 1;
+$("#addTodo").on("click", function() {
+  var newTodo = $("#newTodo").val();
+  if (newTodo === "") {
+    alert("Please add to do item");
+  } else {
+    $("#todoList").append(
+      '<li id="li' +
+        listItemID +
+        '"><p class="todotext">' +
+        newTodo +
+        "</p> " +
+        '<button data-id="li' +
+        listItemID +
+        '" class="removeTodo" href="#/"><i class="fa fa-minus"></i></button> ' +
+        '<button data-id="li' +
+        listItemID +
+        '" class="doneTodo" href="#/"><i class="fa fa-check"></i></button>' +
+        "</li>"
+    );
+    listItemID++;
+
+    //clear input after adding todo
+    $("#newTodo")
+      .val("")
+      .focus();
+  }
+  //save todos to local storage
+  var todos = $("#todoList").html();
+  localStorage.setItem("todos", todos);
+  return false;
+});
+
+//local storage
+if (localStorage.getItem("todos")) {
+  $("#todoList").html(localStorage.getItem("todos"));
+}
+
+//remove todo
+$(document).on("click", ".removeTodo", function() {
+  var checkstr = confirm("Are you sure you want to delete this?");
+  if (checkstr == true) {
+    var removeItemId = "#" + $(this).attr("data-id");
+    $(removeItemId).remove();
+  } else {
+    return false;
+  }
+  //update local storage
+  var todos = $("#todoList").html();
+  localStorage.setItem("todos", todos);
+  return false;
+});
+//todo complete
+$(document).on("click", ".doneTodo", function() {
+  var completeItemId = "#" + $(this).attr("data-id") + " .todotext";
+  $(completeItemId).toggleClass("lineThrough");
+  $(this).toggleClass("doneTodoGrey");
+
+  //update local storage
+  var todos = $("#todoList").html();
+  localStorage.setItem("todos", todos);
+  //return false;
+});
--- usinqubn/routes/index.js
+++ usinqubn/routes/index.js
@@ -19,17 +19,17 @@ router.get("/", ensureGuest, (req, res) => {
 //@route GET /dashboard
 router.get("/dashboard", ensureAuth, async (req, res) => {
   try {
-    const transactionData = await Data.find({ user: req.user.id })//pass in transaction data according to the logged in user
-                                      .lean()
-                                      .sort({date: -1,}) //sort by date                                                                     
+    const transactionData = await Data.find({ user: req.user.id }) //pass in transaction data according to the logged in user
+      .lean()
+      .sort({ date: -1 }); //sort by date
 
-      res.render("dashboard", {  //callbacks
+    res.render("dashboard", {
+      //callbacks
       title: "Dashboard",
       name: req.user.displayName,
       dp: req.user.image,
       transactionData,
-      moment: moment,  
-      
+      moment: moment,
     });
   } catch (err) {
     console.error(err);
@@ -64,9 +64,9 @@ router.post("/", ensureAuth, async (req, res) => {
 //@routes GET /history
 router.get("/history", ensureAuth, async (req, res) => {
   try {
-    const transactionData = await Data.find({ user: req.user.id })//pass in transaction data according to the logged in user
-                                      .lean()
-                                      .sort({date: -1}); //sort by date                              
+    const transactionData = await Data.find({ user: req.user.id }) //pass in transaction data according to the logged in user
+      .lean()
+      .sort({ date: -1 }); //sort by date
     res.render("history", {
       title: "History",
       name: req.user.displayName,
@@ -79,7 +79,6 @@ router.get("/history", ensureAuth, async (req, res) => {
     res.render("errors/500");
   }
 });
-
 
 //@desc Financial-advices
 //@routes GET /financial-advices
@@ -125,6 +124,8 @@ router.get("/monthly", ensureAuth, (req, res) => {
     dp: req.user.image,
   });
 });
+
+// Maybe for the todo list (still not connected to user, only save at local storage even with different users)
 
 //404
 router.get("/404", (req, res) => {
