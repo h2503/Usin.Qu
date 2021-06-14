577479b3a0c057148a971dc79940faae296b3ce8
h2503 <hafiizhamid@gmail.com>
2021-06-14 11:16:01 +0800

    moving out the filter & delete button

--- models/user.js
+++ models/user.js
@@ -7,15 +7,15 @@ const UserSchema = new mongoose.Schema({
     },
     displayName: {
         type: String,
-        
+        //required: true
     },
     firstName: {
         type: String,
-        
+       // required: true
     },
     lastName: {
         type: String,
-        
+        //required: true
     },
     image: {
         type: String,
